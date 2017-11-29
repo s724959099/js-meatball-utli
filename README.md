48ac2c2dfcc64c8c230a02de7132dfd7eb9fb3ce
meatball <mb0520@sinyi-tech.com>
2017-11-29 10:19:08 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# js-meatball-utli
+使用 [webpack config](https://github.com/zhaoshengjun/webpack-config)
---
+++ publish.js
@@ -0,0 +1,27 @@
+
+
+
+let fs = require('fs')
+let cmd = require("node-cmd")
+cmd.run('npm run build')
+
+
+let add_version = (obj) =>{
+  let version = obj.version.split(".")
+  version[2] = parseInt(version[2]) + 1
+  version = version.join(".")
+  obj.version =version
+}
+
+setTimeout(()=>{
+  let obj = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+  add_version(obj)
+  fs.writeFileSync("package.json", JSON.stringify(obj, null, '\t'))
+  cmd.get("npm publish",(err,data)=>{
+    console.log(data)
+    console.log("all done")
+  })
+
+},5000)
+
+
